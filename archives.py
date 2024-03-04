d10f246dc166c176f7a37318f44c2b0d35db8088
maxim.shibaev <shibaiy1991@yandex.ru>
2024-03-04 20:04:32 +0300

    =)

--- archives.py
+++ archives.py
@@ -10,7 +10,7 @@ for i in os.listdir(directory):
     list_files.append(str(Path('.')) + str(Path('/tmp/')) + str(Path('/'))  + i)
 print(list_files)
 
-archive = "test_archive.zip"
+archive = str(Path('./tmp/test_archive.zip'))
 with zipfile.ZipFile(archive,"w") as test_archive:
     for file in list_files:
         test_archive.write(file)
