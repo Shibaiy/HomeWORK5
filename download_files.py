fa608a9795448f91777ef9857ab25e3a8281e538
maxim.shibaev <shibaiy1991@yandex.ru>
2024-03-05 23:59:11 +0300

    =)

--- download_files.py
+++ download_files.py
@@ -1,7 +1,8 @@
-import os
+import  os
 import time
 import requests
 import selene
+import variables as v
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -9,27 +10,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 os.mkdir('tmp')
 
-browser.open("https://github.com/Shibaiy/HomeWORK/blob/main/Files/Xlsx-1.xlsx")
-download_url = selene.browser.element('[data-testid="raw-button"]').get(selene.query.attribute("href"))
-content = requests.get(url=download_url).content
-with open("tmp/xlsx_test.xlsx", 'wb') as file:
-    file.write(content)
+def download_file(file_path):
+    download_url = selene.browser.element('[data-testid="raw-button"]').get(selene.query.attribute("href"))
+    content = requests.get(url=download_url).content
+    with open(file_path, 'wb') as file:
+        file.write(content)
 
-browser.open("https://github.com/Shibaiy/HomeWORK/blob/main/Files/CSV-2.csv")
-download_url = selene.browser.element('[data-testid="raw-button"]').get(selene.query.attribute("href"))
-content = requests.get(url=download_url).content
-with open("tmp/csv_test.csv", 'wb') as file:
-    file.write(content)
+browser.open(v.current_url + v.file_1)
+download_file("tmp/xlsx_test.xlsx")
+
+browser.open(v.current_url +v.file_2)
+download_file("tmp/csv_test.csv")
 
 options = webdriver.ChromeOptions()
 prefs = {
-    "download.default_directory": os.getcwd() + r'\tmp'
+    "download.default_directory": v.download_default_directory
 }
 options.add_experimental_option("prefs", prefs)
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service,options=options)
 browser.config.driver = driver
 
-browser.open("https://github.com/Shibaiy/HomeWORK/blob/main/Files/PDF-3.pdf")
+browser.open(v.current_url + v.file_3)
 browser.element('[data-testid="download-raw-button"]').click()
 time.sleep(3)
---
+++ variables.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import os
+current_url = 'https://github.com/Shibaiy/HomeWORK/blob/main/Files/'
+file_1 = 'Xlsx-1.xlsx'
+file_2 = 'CSV-2.csv'
+file_3 = 'PDF-3.pdf'
+download_default_directory = os.getcwd() + str(Path('/tmp'))
